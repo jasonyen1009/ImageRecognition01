1dc3f2cd3d0513c074df045f706a70efb6578389
Jason <jasonyen1009@gmail.com>
2023-05-21 14:11:24 +0800

    Initial Commit

---
+++ ImageRecognition01/ImageRecognition01App.swift
@@ -0,0 +1,17 @@
+//
+//  ImageRecognition01App.swift
+//  ImageRecognition01
+//
+//  Created by Yen Hung Cheng on 2023/5/21.
+//
+
+import SwiftUI
+
+@main
+struct ImageRecognition01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
